c9bef300929bab4eac7fb08a2220d13361a2b812
Jason Lei <tokenchingy@gmail.com>
2020-05-15 18:33:38 +0800

    Fixed a mistake where I thought I was incrementing by 8 but in actual fact I was incrementing by 7. Never code tired.

--- helpers.js
+++ helpers.js
@@ -110,18 +110,18 @@ const generate15BitColours = () => {
   // The complexity of this is O(N^3).
   for (let i = 0; i < 32; i++) {
     // Increment our red channel intensity.
-    redChannel += 7;
+    redChannel += 8;
 
     for (let j = 0; j < 32; j++) {
       // Increment our green channel intensity.
-      greenChannel += 7;
+      greenChannel += 8;
 
       for (let k = 0; k < 32; k++) {
         // Increment our blue channel intensity.
-        blueChannel += 7;
+        blueChannel += 8;
 
         // Assign the colour to the array of colours.
-        colours[colour] = [redChannel, greenChannel, blueChannel];
+        colours[colour] = [redChannel - 1, greenChannel - 1, blueChannel - 1];
 
         // Increment the colour counter.
         colour++;
